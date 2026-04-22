af60d422f69a66d3e99ec5adf845aa1c0c997a3a
alpharti <aleksmazxd@gmail.com>
2026-04-22 23:39:01 +0200

    Delete CodeLabProject directory

--- CodeLabProject/app/pages/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['logged']) || $_SESSION['logged'] !== true) {
-    header('Location: index.php');
-    exit();
-}
-
-echo "Witaj, ".$_SESSION['login']."! <a href='logout.php'>Wyloguj się</a>";
-?>
